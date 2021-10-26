a866f614193ecfa7b85a862aaad65ebd1d0fb265
Jakub Smekal <jakub.smekal@gmail.com>
2021-10-26 13:57:04 +0100

    WIP: mult_profit_sharing netlist SimStrategy

--- assets/netlists/opsci_mult_profit_sharing/SimStrategy.py
+++ assets/netlists/opsci_mult_profit_sharing/SimStrategy.py
@@ -21,20 +21,19 @@ class SimStrategy(SimStrategyBase.SimStrategyBase):
         self.TRANSACTION_FEES = 0.1
         self.FEES_TO_STAKERS = 0.1
         self.NUMBER_OF_RESEARCHERS = 4
-        '''
-        Some additional parameters that will enable more experimentation (not currently in use)
-        '''
-        self.FUNDING_TIME_DEPENDENCE = True # meaning that TICKS_BETWEEN_PROPOSALS should be used
         self.PROPOSALS_FUNDED_AT_A_TIME = 4 # this would be used if FUNDING_TIME_DEPENDENCE = False, <=> funding as projects finish
         self.PROPOSAL_SETUP = {'grant_requested': 1000, # can be used as a parameter in ResearcherAgent in SimState
                                'assets_generated': 1,
                                'no_researchers': 10}
         self.TREASURY = 'dao_treasury'
-
-        # DT parameters
-        self.DT_init = 100.0
+        '''
+        Some additional parameters that will enable more experimentation (not currently in use)
+        '''
+        self.FUNDING_TIME_DEPENDENCE = True # meaning that TICKS_BETWEEN_PROPOSALS should be used
 
         # DATA TOKEN COMPATIBILITY WIP
+        # DT parameters
+        # self.DT_init = 100.0
         # # pool
         # self.DT_stake = 20.0
         # self.pool_weight_DT    = 3.0
