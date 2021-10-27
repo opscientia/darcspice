70e643808c7e81959d08d1574a8ba1677f0796d6
Jakub Smekal <jakub.smekal@gmail.com>
2021-10-27 10:35:18 +0100

    WIP: MultTimeDAOTreasuryAgent.py done

--- assets/agents/opsci_agents/MultTimeDAOTreasuryAgent.py
+++ assets/agents/opsci_agents/MultTimeDAOTreasuryAgent.py
@@ -34,10 +34,8 @@ class MultTimeDAOTreasuryAgent(AgentBase):
         self._OCEAN_per_grant: float = 0.0
         
         self.proposal_funded = False
-        self.tick_proposal_funded = 0
 
         # metrics to track (and to cross-correlate with ResearcherAgents)
-        self.no_proposals_received: int = 0
         self.total_research_funds_disbursed: float = 0.0
 
     def evaluateProposal(self, state) -> None:
@@ -72,9 +70,16 @@ class MultTimeDAOTreasuryAgent(AgentBase):
             winner = min(scores, key=scores.get)
             self.proposal_evaluation[i] = {'winner': winner, 'amount': state.getAgent(winner).proposal['grant_requested']}
             del scores[winner]
+
+            # immediately disburse funds to new winner
+            self._disburseFundsOCEAN(state, i)
+            self.total_research_funds_disbursed += self.proposal_evaluation[i]['amount']
+            # check if enough OCEAN for next grant
+            if self.OCEAN() < 10000: # arbitrary number
+                break
             if len(self.proposal_evaluation.keys()) == state.ss.PROPOSALS_FUNDED_AT_A_TIME:
                 break
-        assert (len(self.proposal_evaluation.keys()) == state.ss.PROPOSALS_FUNDED_AT_A_TIME)
+        assert (len(self.proposal_evaluation.keys()) <= state.ss.PROPOSALS_FUNDED_AT_A_TIME)
 
     def checkProposalState(self, state):
         '''Checks the currently funded proposals to see whether it is finished or not'''
@@ -91,69 +96,23 @@ class MultTimeDAOTreasuryAgent(AgentBase):
         can_fund = self.proposalsReady(state) and (self.OCEAN() > 10000)
         if not can_fund:
             self.proposal_evaluation = None
+        
+        if (not self.proposal_evaluation) and can_fund:
+            self.evaluateProposal(state)
         # should run only if a proposal_evaluation exists
         if self.proposal_evaluation:
             self.checkProposalState()
             if len(self.proposal_evaluation) < state.ss.PROPOSALS_FUNDED_AT_A_TIME:
                 if can_fund:
                     self.evaluateProposal(state)
-        
-        
+
         #record what we had up until this point
         self._USD_per_tick.append(self.USD())
         self._OCEAN_per_tick.append(self.OCEAN())
                 
-        if (((self.tick_proposal_funded - state.tick) % state.ss.TICKS_BETWEEN_PROPOSALS) == 0) and can_fund:
-            self.proposal_evaluation = self.evaluateProposal(state)
-            self._disburseFundsOCEAN(state)
-            self.tick_proposal_funded = state.tick
-            self.proposal_funded = True
-            self.no_proposals_received += 1
-            for ev in self.proposal_evaluation.keys():
-                self.total_research_funds_disbursed += self.proposal_evaluation[ev]['amount']
-        elif (state.tick == 1 or state.tick == 2) and (self.proposal_funded is False) and can_fund:
-            self.proposal_evaluation = self.evaluateProposal(state)
-            self._disburseFundsOCEAN(state)
-            self.tick_proposal_funded = state.tick
-            self.proposal_funded = True
-            self.no_proposals_received += 1
-            for ev in self.proposal_evaluation.keys():
-                self.total_research_funds_disbursed += self.proposal_evaluation[ev]['amount']
-        
-        # Used for transferring funds to any other agent (not ResearcherAgent)
-        # if self.USD() > 0:
-        #     self._disburseUSD(state)
-        # if self.OCEAN() > 0:
-        #     self._disburseOCEAN(state)
 
-    def _disburseFundsOCEAN(self, state):
-        if self.proposal_evaluation != None:
-            for ev in self.proposal_evaluation.keys():
-                OCEAN = min(self.OCEAN(), self.proposal_evaluation[ev]['amount'])
-                agent = state.getAgent(self.proposal_evaluation[ev]['winner'])
-                self._transferOCEAN(agent, OCEAN)
-    
-    def _disburseFundsUSD(self, state):
-        if self.proposal_evaluation != None:        
-            USD = min(self.USD(), self.proposal_evaluation['amount'])
-            agent = state.getAgent(self.proposal_evaluation['winner'])
-            self._transferUSD(agent, USD)
-
-    def _disburseUSD(self, state) -> None:
-        USD = self.USD()
-        for name, computePercent in self._receiving_agents.items():
-            self._transferUSD(state.getAgent(name), computePercent() * USD)
-
-    def _disburseOCEAN(self, state) -> None:
-        OCEAN = self.OCEAN()
-        for name, computePercent in self._receiving_agents.items():
-            self._transferOCEAN(state.getAgent(name), computePercent() * OCEAN)
-
-    def _tickOneMonthAgo(self, state) -> int:
-        t2 = state.tick * state.ss.time_step
-        t1 = t2 - S_PER_MONTH
-        if t1 < 0:
-            return 0
-        tick1 = int(max(0, math.floor(t1 / float(state.ss.time_step))))
-        return tick1
-        
+    def _disburseFundsOCEAN(self, state, i):
+        assert self.proposal_evaluation
+        OCEAN = min(self.OCEAN(), self.proposal_evaluation[i]['amount'])
+        agent = state.getAgent(self.proposal_evaluation[i]['winner'])
+        self._transferOCEAN(agent, OCEAN)
