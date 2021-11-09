568a224eff91b5b08271c2730f4a43be0724a69d
Jakub Smékal <jakub.smekal@gmail.com>
2021-11-09 18:10:49 +0000

    Working fixed mult_time_profit_sharing model

--- assets/netlists/opsci_mult_time_profit_sharing/SimState.py
+++ assets/netlists/opsci_mult_time_profit_sharing/SimState.py
@@ -43,11 +43,6 @@ class SimState(SimStateBase.SimStateBase):
         new_agents = []
 
         #################### Wiring of agents that send OCEAN ####################
-        new_agents.append(MultTimeKnowledgeMarketAgent(
-            name = "market", USD=0.0, OCEAN=0.0,
-            transaction_fees_percentage=0.1,
-            fee_receiving_agents={"staker": self.ss.FEES_TO_STAKERS, "dao_treasury": 1.0 - self.ss.FEES_TO_STAKERS}))
-
         new_agents.append(MultTimeDAOTreasuryAgent(
             name = "dao_treasury", USD=0.0, OCEAN=500000.0))
 
@@ -63,6 +58,11 @@ class SimState(SimStateBase.SimStateBase):
                 name = "researcher%x" % i, evaluator = "dao_treasury",
                 USD=0.0, OCEAN=10000.0,
                 receiving_agents = {"market": 1.0}))
+        
+        new_agents.append(MultTimeKnowledgeMarketAgent(
+            name = "market", USD=0.0, OCEAN=0.0,
+            transaction_fees_percentage=0.1,
+            fee_receiving_agents={"staker": self.ss.FEES_TO_STAKERS, "dao_treasury": 1.0 - self.ss.FEES_TO_STAKERS}))
 
         for agent in new_agents:
             self.agents[agent.name] = agent            
