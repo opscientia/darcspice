8e32154ee678d5027d57dbf91c37bddb6d91dcfe
Jakub Smékal <jakub.smekal@gmail.com>
2021-11-18 21:15:36 +0000

    init ResearcherGenerator

---
+++ assets/agents/opsci_pp_agents/ResearcherGenerator.py
@@ -0,0 +1,32 @@
+import logging
+log = logging.getLogger('agents')
+
+from enforce_typing import enforce_types
+import random
+
+from assets.agents.opsci_pp_agents.VersatileResearcherAgent import VersatileResearcherAgent
+from engine.AgentBase import AgentBase
+from web3tools.web3util import toBase18
+                    
+@enforce_types
+class ResearcherGeneratorAgent(AgentBase):
+    """Community growth agent"""
+    def __init__(self, name: str, USD: float, OCEAN: float):
+        super().__init__(name, USD, OCEAN)   
+        self.generated_agents_idx = 0
+        
+    def takeStep(self, state):
+        if self._doCreateVersatileResearcherAgent(state):
+            self._createVersatileResearcherAgent(state)
+
+    def _doCreateVersatileResearcherAgent(self, state) -> bool:
+        return state.getAgent(self._evaluator).update > 0
+
+    def _createVersatileResearcherAgent(self, state) -> None:
+        name = f'new_researcher_{self.generated_agents_idx}'
+        self.generated_agents_idx += 1
+        USD = 0.0 # magic number
+        OCEAN = 200000.0 # same as other researchers
+        new_agent = VersatileResearcherAgent(name=name, USD=USD, OCEAN=OCEAN)
+        state.addAgent(new_agent)
+        state.addResearcherAgent(new_agent)
