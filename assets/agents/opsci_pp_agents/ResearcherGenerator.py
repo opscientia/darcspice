736c4b08a9ee03612c16b758b8d02b8dcd0c8b86
Jakub Smékal <jakub.smekal@gmail.com>
2021-11-21 16:09:48 +0000

    WIP: community growth time-dependent

--- assets/agents/opsci_pp_agents/ResearcherGenerator.py
+++ assets/agents/opsci_pp_agents/ResearcherGenerator.py
@@ -11,10 +11,11 @@ from web3tools.web3util import toBase18
 @enforce_types
 class ResearcherGeneratorAgent(AgentBase):
     """Community growth agent"""
-    def __init__(self, name: str, evaluator: str, USD: float, OCEAN: float):
+    def __init__(self, name: str, evaluator: str, USD: float, OCEAN: float, time_interval: int):
         super().__init__(name, USD, OCEAN)   
         self.generated_agents_idx = 0
         self._evaluator = evaluator
+        self.time_interval = time_interval
         
     def takeStep(self, state):
         if self._doCreateVersatileResearcherAgent(state):
@@ -22,6 +23,9 @@ class ResearcherGeneratorAgent(AgentBase):
 
     def _doCreateVersatileResearcherAgent(self, state) -> bool:
         return state.getAgent(self._evaluator).update > 0
+    
+    def _doCreateVersatileResearcherAgentTime(self, state) -> bool:
+        return state.tick % self.time_interval == 0
 
     def _createVersatileResearcherAgent(self, state) -> None:
         name = f'new_researcher_{self.generated_agents_idx}'
