859c108d4040af046fea1fc8af6c17543c8936aa
Jakub Smékal <jakub.smekal@gmail.com>
2021-11-21 16:17:28 +0000

    WIP: added actions to ResearcherGeneratorAgent

--- assets/agents/opsci_pp_agents/ResearcherGenerator.py
+++ assets/agents/opsci_pp_agents/ResearcherGenerator.py
@@ -11,17 +11,28 @@ from web3tools.web3util import toBase18
 @enforce_types
 class ResearcherGeneratorAgent(AgentBase):
     """Community growth agent"""
-    def __init__(self, name: str, evaluator: str, USD: float, OCEAN: float, time_interval: int):
+    def __init__(self, name: str, evaluator: str, USD: float, OCEAN: float, generator_type: str, time_interval: int):
         super().__init__(name, USD, OCEAN)
 
         self.generated_agents_idx = 0
         self._evaluator = evaluator
         self.time_interval = time_interval
         self.market_update = 0
-        
+        self.generator_type = generator_type
+
     def takeStep(self, state):
-        if self._doCreateVersatileResearcherAgentTreasury(state):
-            self._createVersatileResearcherAgent(state)
+
+        if self.generator_type == "treasury":
+            if self._doCreateVersatileResearcherAgentTreasury(state):
+                self._createVersatileResearcherAgent(state)
+        elif self.generator_type == "market":
+            if self._doCreateVersatileResearcherAgentMarket(state):
+                self._createVersatileResearcherAgent(state)
+        elif self.generator_type == "time":
+            if self._doCreateVersatileResearcherAgentTime(state):
+                self._createVersatileResearcherAgent(state)
+
+
 
     def _doCreateVersatileResearcherAgentTreasury(self, state) -> bool:
         return state.getAgent(self._evaluator).update > 0
