50bb907c9f634dcd43788b574d849177a7836b1d
Jakub Smékal <jakub.smekal@gmail.com>
2021-11-25 19:33:13 +0000

    WIP: added new plot to KPIs

--- assets/netlists/opsci_mult_time_profit_sharing/KPIs.py
+++ assets/netlists/opsci_mult_time_profit_sharing/KPIs.py
@@ -112,8 +112,9 @@ def netlist_plotInstructions(header: List[str], values):
     y_params = [
         YParam(proposals_funded,
         researchers,"#_proposals_FUNDED",LINEAR,MULT1,COUNT),
-        YParam(["dao_treasury_funded_proposals_integration", "dao_treasury_funded_proposals_novelty", "dao_treasury_funded_proposals_in_index", "funded_proposals_impact"],
-        ["integration", "novelty", "in_index", "impact"],"integration, novelty, impact in funded proposals",LINEAR,MULT1,COUNT),
+        YParam(["dao_treasury_funded_proposals_integration", "dao_treasury_funded_proposals_novelty", "dao_treasury_funded_proposals_in_index"],
+        ["integration", "novelty", "in_index"],"integration vs novelty in funded proposals",LINEAR,MULT1,COUNT),
+        YParam(["funded_proposals_impact"],["impact"], "impact of funded proposals",LINEAR,MULT1,COUNT),
         YParam(proposals,
         researchers,"#_proposals",LINEAR,MULT1,COUNT),
         YParam(total_funding,
