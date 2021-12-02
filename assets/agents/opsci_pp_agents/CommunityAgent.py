39ae3aeb982a80aa26a9620d89bfd43d4c11bd9f
Jakub Smékal <jakub.smekal@gmail.com>
2021-12-02 10:25:53 +0000

    WIP: init CommunityAgent

---
+++ assets/agents/opsci_pp_agents/CommunityAgent.py
@@ -0,0 +1,42 @@
+import logging
+log = logging.getLogger('agents')
+
+from enforce_typing import enforce_types
+from typing import List, Dict
+import random
+
+from engine.AgentBase import AgentBase
+from util.constants import S_PER_MONTH
+
+@enforce_types
+class CommunityAgent(AgentBase):
+    '''
+    Participates in the knowledge market, integrates knowledge assets, receives rewards
+    '''
+    def __init__(self, name: str, USD: float, OCEAN: float):
+        super().__init__(name, USD, OCEAN)
+
+        #track amounts over time
+        self.knowledge_access: int = 0
+        self.curiosity: float = 1
+
+    def takeStep(self, state) -> None:
+        self.treasury = state.getAgent(state.ss.TREASURY)
+
+        if self.treasury.update > 0:
+            self._getCuriosity(state)
+            if self.curiosity > 0.5: # magic number
+                self._interact(state)
+
+    def _getCuriosity(self) -> None:
+        assert self.treasury.proposal_evaluation != {}
+
+        self.curiosity = (self.treasury.impact / 10) + random.randrange(-0.5, 0.5)
+        if self.curiosity > 1:
+            self.curiosity = 1
+        if self.curiosity < 1:
+            self.curiosity = 0
+
+    def _interact(self, state):
+        self._transferOCEAN('public_market', state.ss.PRICE_OF_ASSETS)
+        self.knowledge_access += 1
