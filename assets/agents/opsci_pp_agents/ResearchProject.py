514b5d31d36795f4254a0f90970ac8bb20403896
Jakub Smékal <jakub.smekal@gmail.com>
2021-12-06 10:52:26 +0000

    init ResearchProject class

---
+++ assets/agents/opsci_pp_agents/ResearchProject.py
@@ -0,0 +1,23 @@
+import logging
+log = logging.getLogger('agents')
+
+from enforce_typing import enforce_types
+from typing import List, Dict
+import random
+
+from util.constants import S_PER_MONTH
+
+@enforce_types
+class ResearchProject():
+    '''
+    Represents a funded research project. Created by Researchers, used for tracking community engagement, academic lineage, impact, etc.
+    '''
+    def __init__(self, name: str, creator: str, value: float, impact: float, integration: float, novelty: float, engagement: float):
+
+        self.name = name
+        self.creator = creator
+        self.value = value
+        self.impact = impact
+        self.integration = integration
+        self.novelty = novelty
+        self.engagement = engagement
