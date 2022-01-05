5c05f3a1476352864b2fbbacea5ae107ee9b14c9
Jakub Smékal <jakub.smekal@gmail.com>
2022-01-05 11:11:55 +0100

    WIP: added rp logging & plotting to all KPIs.py

--- assets/netlists/growth_public_funding_ps/KPIs.py
+++ assets/netlists/growth_public_funding_ps/KPIs.py
@@ -254,3 +254,69 @@ def netlist_plotInstructions(header: List[str], values):
     ]
 
     return (x, y_params)
+
+@enforce_types
+def netlist_rp_createLogData(state):
+    """pass this to SimEngine.__init__() as argument `netlist_createLogData`"""
+    kpis = state.kpis
+    dataheader = [] # for csv logging: list of string
+    datarow = [] #for csv logging: list of float
+
+    dataheader += ["total_rp"]
+    datarow += [kpis._total_rp[-1]]
+    dataheader += ["total_rp_engagement"]
+    datarow += [kpis._total_rp_engagement[-1]]
+    dataheader += ["total_rp_integration"]
+    datarow += [kpis._total_rp_integration[-1]]
+    dataheader += ["total_rp_novelty"]
+    datarow += [kpis._total_rp_novelty[-1]]
+    dataheader += ["total_rp_impact"]
+    datarow += [kpis._total_rp_impact[-1]]
+
+    for rp, spec in state.projects.items():
+        dataheader += ["%s_impact" % rp]
+        datarow += [spec.impact]
+        dataheader += ["%s_engagement" % rp]
+        datarow += [spec.engagement]
+
+    return dataheader, datarow
+
+@enforce_types
+def netlist_rp_plotInstructions(header: List[str], values):
+    """
+    Describe how to plot the information.
+    tsp.do_plot() calls this
+
+    :param: header: List[str] holding 'Tick', 'Second', ...
+    :param: values: 2d array of float [tick_i, valuetype_i]
+    :return: x: List[float] -- x-axis info on how to plot
+    :return: y_params: List[YParam] -- y-axis info on how to plot
+    """
+    from util.plotutil import YParam, arrayToFloatList, \
+        LINEAR, LOG, BOTH, \
+        MULT1, MULT100, DIV1M, DIV1B, \
+        COUNT, DOLLAR, PERCENT
+    
+    x = arrayToFloatList(values[:,header.index("Month")])
+    rp_list = [e for e in header if 'researcher' in e]
+    impact = [p for p in rp_list if '_impact' in p]
+    engagement = [p for p in rp_list if '_engagement' in p]
+    
+    y_params = [
+        YParam(["total_rp"],
+        ["total"],"Total projects funded",LINEAR,MULT1,COUNT),
+        YParam(["total_rp_engagement"],
+        ["total"],"Total project engagement",LINEAR,MULT1,COUNT),
+        YParam(["total_rp_integration"],
+        ["total"],"Total project integration",LINEAR,MULT1,COUNT),
+        YParam(["total_rp_novelty"],
+        ["total"],"Total project novelty",LINEAR,MULT1,COUNT),
+        YParam(["total_rp_impact"],
+        ["total"],"Total project impact",LINEAR,MULT1,COUNT),
+        YParam(impact,
+        impact,"Research projects impact",LINEAR,MULT1,COUNT),
+        YParam(engagement,
+        engagement,"Research projects engagement",LINEAR,MULT1,COUNT),
+    ]
+
+    return (x, y_params)
